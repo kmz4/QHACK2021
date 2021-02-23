a516eceda5beb6f54e074feb7a101e697cf35ca4
rooler <roel.wier@gmail.com>
2021-02-23 13:56:03 -0500

    Added seperate folder for the final code

---
+++ src/driver_code.py
@@ -0,0 +1,16 @@
+from subarchitecture_tree_search import run_tree_architecture_search
+
+
+if __name__=="__main__":
+
+    config = {'nqubits': 3,
+              'nclasses': 2,
+              'loss_function': 'hinge',
+              'min_tree_depth':3,
+              'max_tree_depth': 8,
+              'prune_rate': 0.3,
+              'prune_step': 3,
+              'plot_trees': False,
+              'data_set': 'moons'
+              }
+    run_tree_architecture_search(config)
