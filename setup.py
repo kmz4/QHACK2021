2e7182c2606eff9f35bc8aa46c92bb645e696c0a
Kathleen Hamilton <kathleenhamilton@Kathleens-iMac.local>
2021-02-24 07:33:40 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+# placeholder for now
+setup(
+    name='',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Aroosa Ijaz, Kathleen Hamilton, Jelena Mackeprang,Roeland Wiersema',
+    author_email='',
+    description='',
+    install_requires=['numpy', 'pennylane', 'torch', 'autograd'],
+    python_requires='>=3.6',
+    package_dir = {'': ''},
+)
+
